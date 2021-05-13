a9c42b1ae161a1677d724bc938877c4c389936bf
fate411 <74136177+fate411@users.noreply.github.com>
2021-05-13 22:40:48 +0800

    Update jd_jxnc.js

--- jd_jxnc.js
+++ jd_jxnc.js
@@ -37,8 +37,8 @@ let tokenNull = {'farm_jstoken': '', 'phoneid': '', 'timestamp': ''}; // 内置�
 let tokenArr = []; // 用户 token 数组
 let currentToken = {}; // 当前用户 token
 let shareCode = ''; // 内置助力码
-let jxncShareCodeArr = []; // 用户 助力码 数组
-let currentShareCode = [3c811619ecc568ea82ef61aa3a9390d9]; // 当前用户 要助力的助力码
+let jxncShareCodeArr = [{"smp":"3c811619ecc568ea82ef61aa3a9390d9","active":"jdnc_1_3yuanliulianb210422_2","joinnum":1}]; // 用户 助力码 数组
+let currentShareCode = [{"smp":"3c811619ecc568ea82ef61aa3a9390d9","active":"jdnc_1_3yuanliulianb210422_2","joinnum":1}]; // 当前用户 要助力的助力码
 const openUrl = `openjd://virtual?params=${encodeURIComponent('{ "category": "jump", "des": "m", "url": "https://wqsh.jd.com/sns/201912/12/jxnc/detail.html?ptag=7155.9.32&smp=b47f4790d7b2a024e75279f55f6249b9&active=jdnc_1_chelizi1205_2"}',)}`; // 打开京喜农场
 let subTitle = '', message = '', option = {'open-url': openUrl}; // 消息副标题，消息正文，消息扩展参数
 const JXNC_API_HOST = 'https://wq.jd.com/';
